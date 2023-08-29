5e53d05486d885386bfa5979edbbd1236d5e6ffe
Wei jun Lim <welim@u90z03s01.42kl.edu.my>
2023-08-29 18:05:02 +0800

    adding files

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -5,8 +5,8 @@ int main()
 	FragTrap Dan("Dan");
 
 	Dan.attack("enemy");
-	// Dan.takeDamage(10);
-	// Dan.beRepaired(500);
+	Dan.takeDamage(10);
+	Dan.beRepaired(500);
 	Dan.highFivesGuys();
 	return (0);
 }
